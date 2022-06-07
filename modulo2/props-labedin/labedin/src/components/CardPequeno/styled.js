e5d67395c6790265b83f97e3b4b964c925c94415
Douglas Henrique <douggx10@Hotmail.com>
2022-06-07 19:31:13 -0300

    labedin

---
+++ modulo2/props-labedin/labedin/src/components/CardPequeno/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const LittleCardContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 100px;
+`
+
+export const LittleCardImg = styled.img`
+    width: 40px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+
+export const LittleCardH4 = styled.h4`
+    margin-bottom: 15px;
+`
+
+export const OutroLittleCardContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-items: flex-start;
+`;
