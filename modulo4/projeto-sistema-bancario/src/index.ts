4792a0fa1fe9eaa96958b0cc32098709b45c226d
Douglas Henrique <douggx10@Hotmail.com>
2022-08-21 19:13:02 -0300

    projeto-sistema-bancario

--- modulo4/projeto-sistema-bancario/src/index.ts
+++ modulo4/projeto-sistema-bancario/src/index.ts
@@ -1,134 +1,79 @@
-import express, { Request, Response } from 'express'
+import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
-import { users } from './data';
-const app = express();
+import { accounts } from './data';
+
+const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/users/create", (req: Request, res: Response) => {
+    try {
+       const { name, CPF, dateOfBirthAsString } = req.body
 
+        const [day, month, year] = dateOfBirthAsString.split("/")
 
-// app.get('/test/hello', (req: Request, res: Response) => {
-//     res.send(`Olá, mundo!`)
-// })
-// app.get('/test/:number', (req: Request, res: Response) => {
-//     res.send(
-//         `Seu número da sorte é ${Number(req.params.number) + 3}!
-//     `)
-// })
+       const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
 
+       const ageInMilisseconds: number = Date.now() - dateOfBirth.getTime()
+       const ageInYears: number = ageInMilisseconds / 1000 / 60 / 60 / 24 / 365
 
+       if (ageInYears < 18) {
+        res.statusCode = 406
+        throw new Error("Idade deve ser maior que 18.")
+       }
 
-app.get("/playlists", (req: Request, res: Response) => {
-    const playlists = users.map((user) => {
-        return user.playlists
-    }).flat()
-    res.send(playlists)
-})
-app.get("/tracks", (req: Request, res: Response) => {
-    const playlists = users.map((user) => {
-        return user.playlists
-    }).flat()
-    const tracks = playlists.map((playlist) => {
-        return playlist.tracks
-    }).flat()
-    res.send(tracks)
-})
-app.delete("/playlist/:playlistId", (req: Request, res: Response) => {
-    const userId = req.headers.user
-    const playlistId = req.params.playlistId
-    // console.log("userId", userId)
-    const user = users.find((user) => {
-        return user.id === userId
-    })
-    if (user === undefined) {
-        res.status(404).send("Usuario não encontrado")
-        return
-    }
-    user.playlists = user?.playlists.filter((playlist) => {
-        return playlist.id !== playlistId
-    })
-    res.end()
-})
-app.delete("/track/:trackId", (req: Request, res: Response) => {
-    const userId = req.headers.user
-    const trackId = req.params.trackId
-    const user = users.find((user) => user.id === userId)
-    if (user === undefined) {
-        res.status(404).send("Usuario não foi encontrado")
-        return
-    }
-    user.playlists = user.playlists.map((playlist) => {
-        // const newPlaylist =
-        return {
-            id: playlist.id,
-            name: playlist.name,
-            tracks: playlist.tracks.filter((track) => {
-                return track.id !== trackId
-            })
+       const cpfFind = accounts.find((account) => account.CPF === CPF)
+
+       if (cpfFind) {
+            res.statusCode = 401
+            throw new Error ("CPF já existe!")
         }
-    })
-    res.end()
-})
-app.get("/playlists/search", (req: Request, res: Response): void => {
-    const name = req.query.name as string
 
-    const allPlaylists = users.map((user) => {
-        return user.playlists
-    }).flat()
+       accounts.push({
+        name,
+        CPF,
+        dateOfBirth,
+        balance: 0,
+        statement: []
+       })
 
-    const result = allPlaylists.filter((playlist) => {
-        return name ? playlist.name.includes(name) : true;
-    })
-    res.send(result)
-})
-app.post("/playlists", (req: Request, res: Response) => {
-    const { idUser, name } = req.body
-    const user = users.find((user) => user.id === idUser)
-
-    if (user) {
-        user.playlists.push({
-            id: Date.now().toString(),
-            name: name,
-            tracks: []
-        })
-
-        res.send("Playlist foi criada")
-    } else {
-        res.status(400).send("Usuario não encontrado")
+       res.status(201).send("Conta criada com sucesso!")
+    } catch (error:any) {
+        res.send(error.message)
     }
-
 })
 
-app.post("/playlists/:playlistId/tracks", (req: Request, res: Response) => {
-    
-    const { name, artist, url } = req.body
-
-    const playlistId = req.params.playlistId
-    const userId = req.headers.authorization
-
-    const user = users.find((user) => user.id === userId)
-
-    if (user) {
-        const playlist = user.playlists.find((playlist) => {
-            return playlist.id === playlistId
-        })
-        if(playlist){
-            playlist.tracks.push({
-                id: Date.now().toString(),
-                name,
-                artist,
-                url
-            })
-            res.send("Track foi criada")
-        }else{
-            res.status(404).send("Playlist não encontrada")
+app.get("/users/all", (req: Request, res: Response) => {
+    try {
+        if (!accounts.length){
+            res.statusCode = 404
+            throw new Error ("nenhuma conta encontrada")
         }
-    } else {
-        res.status(404).send("Usuario não encontrado")
+        res.status(200).send(accounts)
+    } catch (error: any) {
+        res.send(error.message)
     }
-
 })
 
-app.listen(3003, function () {
-    console.log("Meu servidor está rodando na porta 3003");
+app.get("/users/getUserByCPF", (req: Request, res: Response) => {
+    try {
+        const {CPF} = req.body 
+
+        const accountCPF = accounts.find((account) => account.CPF === CPF)
+
+        if(accountCPF?.CPF === CPF){
+            res.status(200).send(accountCPF)
+        }
+
+        if(!accountCPF){
+            res.status(404).send("CPF não encontrado")
+        }
+
+    } catch (error: any) {
+        res.send(error.message)
+    }
+})
+
+app.listen(3003, () => {
+    console.log("Servidor está rodando na porta 3003");
 });
